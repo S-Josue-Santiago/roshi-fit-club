6efe1f2a6b222daefe605d8defc6eb7bb6ec8493
S-Josue-Santiago <saquicusjosuesanti@gmail.com>
2025-10-23 17:07:12 -0600

    cambios necesarios para vercel

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crea una instancia de Axios con una URL base.
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // OK si tu backend usa 5000
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
